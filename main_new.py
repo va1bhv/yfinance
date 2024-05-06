3d867688c85e130d10139718c93bd6d56df3aee2
Vaibhav <vaibhv007@gmail.com>
2024-05-07 01:55:28 +0530

    Fifth commit

--- main_new.py
+++ main_new.py
@@ -1,6 +1,7 @@
 import numpy as np
 import pandas as pd
 import yfinance as yf
+from statsmodels.tsa.arima.model import ARIMA
 from tqdm import tqdm
 
 tqdm.pandas()
@@ -88,52 +89,83 @@ for i in tqdm(range(len(tickers))):
 tk = tickers.iloc[6, 0]
 df = get_data(tk)
 
-# a = get_data('TATAPOWER.NS')
 
-import matplotlib.pyplot as plt
-from statsmodels.graphics.tsaplots import plot_acf, plot_pacf
-from statsmodels.tsa.stattools import adfuller
-from statsmodels.tsa.arima.model import ARIMA
+# a = get_data('TATAPOWER.NS')
 
 
 # Function to determine the order of ARIMA model
-def find_arima_order(time_series):
-    # Step 2: Check stationarity
-    result = adfuller(time_series)
-    stationary = False
-    if result[1] < 0.05:  # p-value threshold
-        stationary = True
-        d = 0
-    else:
-        d = 1
-
-    # Step 3: Plot ACF and PACF
-    if stationary:
-        plot_acf(time_series)
-        plot_pacf(time_series)
-        plt.show()
-
-    # Step 4: Grid search for p and q
-    best_aic = np.inf
-    best_order = None
-    for p in range(3):
-        for q in range(3):
-            try:
-                model = ARIMA(time_series, order=(p, d, q))
-                fitted_model = model.fit()
-                aic = fitted_model.aic
-                if aic < best_aic:
-                    best_aic = aic
-                    best_order = (p, d, q)
-            except:
-                pass
-
-    return best_order
+# def find_arima_order(time_series):
+#     # Step 2: Check stationarity
+#     result = adfuller(time_series)
+#     stationary = False
+#     if result[1] < 0.05:  # p-value threshold
+#         stationary = True
+#         d = 0
+#     else:
+#         d = 1
+#
+#     # Step 3: Plot ACF and PACF
+#     if stationary:
+#         plot_acf(time_series)
+#         plot_pacf(time_series)
+#         plt.show()
+#
+#     # Step 4: Grid search for p and q
+#     best_aic = np.inf
+#     best_order = None
+#     for p in range(3):
+#         for q in range(3):
+#             try:
+#                 model = ARIMA(time_series, order=(p, d, q))
+#                 fitted_model = model.fit()
+#                 aic = fitted_model.aic
+#                 if aic < best_aic:
+#                     best_aic = aic
+#                     best_order = (p, d, q)
+#             except:
+#                 pass
+#
+#     return best_order
 
 
 # Example usage:
-a = get_data('SBIN.NS')
+# a = get_data('SBIN.NS')
+# a.reset_index(drop=True, inplace=True)
+# ts = a.loc[:, '%Diff'].values
+# order = find_arima_order(ts)
+# print("Best ARIMA order:", order)
+
+# Order to be used will be (1, 1, 0)
+
+def predict_next_number_arima(time_series):
+    """
+    Predict the next number in a time series using the ARIMA model.
+
+    Parameters:
+        time_series (list): A list of numbers representing the time series.
+
+    Returns:
+        float: The predicted next number in the time series.
+    """
+    # Convert the time series to numpy array
+    time_series = np.array(time_series)
+
+    # Fit ARIMA model
+    model = ARIMA(time_series, order=(1, 1, 0))  # Example order, you may need to adjust
+    fitted_model = model.fit()
+
+    # Forecast the next value
+    forecast = fitted_model.forecast(steps=1)
+
+    # Extract the forecasted value
+    next_number = forecast[0]
+
+    return next_number
+
+
+# Example usage:
+a = get_data('TATAPOWER.NS')
 a.reset_index(drop=True, inplace=True)
-ts = a.loc[:, '%Diff'].values
-order = find_arima_order(ts)
-print("Best ARIMA order:", order)
+ts = a.loc[range(4, 19), '%Diff'].values
+nn = predict_next_number_arima(ts)
+print("Next number prediction (ARIMA):", nn)
