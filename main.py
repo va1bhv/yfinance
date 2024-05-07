d1bdc4c358cc236df8f84269461935b18450f6eb
Vaibhav <vaibhv007@gmail.com>
2024-05-07 15:01:48 +0530

    Added final data

--- main.py
+++ main.py
@@ -18,21 +18,21 @@ tickers = pd.read_excel("MCAP28032024.xlsx")
 tickers = tickers.drop(columns=['Sr. No.'])
 tickers.rename(columns={'Market capitalization as on March 28, 2024\n(In lakhs)': 'Market Cap'}, inplace=True)
 tickers['Symbol'] = tickers['Symbol'] + '.NS'
-tickers[['Close to crossover', 'Next Diff', 'RSI']] = 0
+tickers[['Close to crossover', 'Next Diff', 'RSI']] = False
 tickers.set_index('Symbol', drop=False, inplace=True)
 tickers_top500 = tickers.nlargest(500, 'Market Cap')
 collection_period = '5d'
 collection_interval = '90m'
-data: pd.DataFrame = yf.download(tickers=tickers_top500['Symbol'].to_list(),
-                                 period=collection_period,
-                                 interval=collection_interval,
-                                 group_by='ticker',
-                                 threads=True)
-data.reset_index(drop=False, inplace=True, names='DateTime')
+raw_data: pd.DataFrame = yf.download(tickers=tickers_top500['Symbol'].to_list(),
+                                     period=collection_period,
+                                     interval=collection_interval,
+                                     group_by='ticker',
+                                     threads=True)
+raw_data.reset_index(drop=False, inplace=True, names='DateTime')
 
 
 def get_data(tkr, rolling_up=20, rolling_down=5) -> pd.DataFrame:
-    dt = data[tkr]
+    dt = raw_data[tkr]
     dt['MA5'] = dt['Close'].rolling(rolling_down).mean()
     dt['MA20'] = dt['Close'].rolling(rolling_up).mean()
     dt['%Diff'] = ((dt['MA5'] - dt['MA20']) / dt['MA5']) * 100
@@ -62,16 +62,16 @@ def rsi(dta, window=14, adjust=False) -> float:
 
 def compute(tkr: str) -> tuple[bool, float, float]:
     """
-    :param tkr: Symbol to fetch the data for
+    :param tkr: Symbol to fetch the raw_data for
     :return close_to_crossover: if the difference between the moving averages will cross over
             nn: forecasted difference next in the time series
-            rsi: the computed rsi of the latest data
+            rsi: the computed rsi of the latest raw_data
     """
     temp = get_data(tkr)
     temp.reset_index(inplace=True, names='DateTime')
     time_series = temp['%Diff'].values
 
-    if any(time_series[-3:] < 0):
+    if temp.empty or any(time_series[-3:] < 0):
         return False, -1, -1
 
     nn = ARIMA(time_series, order=(1, 1, 0)).fit().forecast(steps=1)[0]
@@ -83,3 +83,5 @@ def compute(tkr: str) -> tuple[bool, float, float]:
 
 for smbl in tqdm(tickers_top500.index, total=len(tickers_top500)):
     tickers_top500.loc[smbl, ['Close to crossover', 'Next Diff', 'RSI']] = compute(smbl)
+
+tickers_top500 = tickers_top500[tickers_top500['Close to crossover']].sort_values(by='RSI', ascending=False)
